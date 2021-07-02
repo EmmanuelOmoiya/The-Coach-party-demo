7ea3c447f9d886946a21aa295d9c9b748b97b556
Emmanuel Omoiya <omoiyaemmanuel@yahoo.com>
2021-07-02 06:07:43 +0100

    Verge of knowledge

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,17 @@
 //server/index.js
-const express =require("express")
+const express =require("express");
 const PORT = process.env.PORT || 4433;
 const app = express();
+const path = require('path');
 
-app.use(express.static(path.resolve(_dirname,'../react-tutorial/build')));
+app.use(express.static(path.resolve(__dirname,'../client/build')));
 
 app.get("/api", (req, res) =>{
     res.json({message: "Hello from server!"});
 });
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(_dirname,'../react-tutorial', 'index.html'));
+    res.sendFile(path.resolve(_dirname,'../client/build', 'index.html'));
 });
 
 app.listen(PORT,() =>{
